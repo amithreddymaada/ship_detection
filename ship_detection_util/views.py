65ad30496a26a96142ff87ecb5e9af9fd1d1fa28
Amith Reddy Maada <amithreddymada@gmail.com>
2022-03-16 17:42:59 +0530

    Created template for the project

--- ship_detection_util/views.py
+++ ship_detection_util/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    return render(request,'ship_detection_util/index.html', {"title":"Ship DD"})
